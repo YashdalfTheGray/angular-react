b2c190d71896a28776bc9cb777ffe96de3204775
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-05-29 18:57:43 +0530

    added table rendering example

--- src/angular-app.js
+++ src/angular-app.js
@@ -1,23 +1,91 @@
 /* global angular */
 
 angular.module('rectangular', [])
-.controller('MainCtrl', 
+.controller('SingleCtrl', 
 	[
 		'$scope', 
 		function($scope){
 			
 		}
 	]
-)
-.directive('reactDirective', function() {
-	return {
-		scope: {
-			framework: '='
-		},
-		link: function reactDirectiveLink (scope, elem, attr) {
-			scope.$watch('framework', function (newValue, oldValue) {
-				React.render(React.createFactory(aReactComp)({ framework: newValue }), elem[0]);
-			});
+).controller('TableCtrl', 
+	[
+		'$scope', 
+		function ($scope){
+			$scope.renderAngular = true;
+			$scope.framework = 'ReactJS';
+			$scope.data = [];
+
+			$scope.switchRendering = function() {
+				$scope.renderAngular = !$scope.renderAngular;
+				$scope.framework = $scope.renderAngular ? 'ReactJS' : 'AngularJS';
+				$scope.refresh();
+			};
+			$scope.refresh = function(){
+				for(var i = 0; i < 1500; ++i) {
+					$scope.data[i] = {};
+					for(var j = 0; j < 5; ++j) {
+						$scope.data[i][j] = Math.random();
+					}
+					$scope.data[i].key = 'row_' + i;
+				}
+			};
+
+			$scope.refresh();	
 		}
-	};
-});
+	]
+)
+.directive('simpleRender', 
+	[
+		function() {
+			return {
+				scope: {
+					framework: '='
+				},
+				link: function simpleRenderLink (scope, elem) {
+					scope.$watch('framework', function (newValue, oldValue) {
+						React.render(React.createFactory(simpleRender)({ framework: newValue }), elem[0]);
+					});
+				}
+			};
+		}
+	]
+)
+.directive('ngRepeatTable', 
+	[
+		function(){
+			return {
+				scope: {
+					data: '='
+				},
+				restrict: 'E',
+				template: 	'<table>' +
+	          				  	'<tr ng-repeat="line in data">' +
+	            					'<td>{{line[0]}}</td>' +
+	            					'<td>{{line[1]}}</td>' +
+	            					'<td>{{line[2]}}</td>' +
+	        						'<td>{{line[3]}}</td>' +
+	            					'<td>{{line[4]}}</td>' +
+	          					'</tr>' +
+        					'</table>'
+			};
+		}
+	]
+)
+.directive('reactTableRender', 
+	[
+		function(){
+			return {
+				scope: {
+					data: '=',
+				},
+				restrict: 'E',
+				link: function(scope, elem) {
+					scope.$watchCollection('data', function (newValue, oldValue) {
+						React.render(React.createFactory(tableRender)({ data: newValue }), elem[0]);
+					});
+				}
+			};
+		}
+	]
+);
--- src/react-app.js
+++ src/react-app.js
@@ -1,8 +1,42 @@
-var aReactComp = React.createClass(
+var simpleRender = React.createClass(
 	{
-		displayName:'aReactComp',
-		render:function() {
+		displayName: 'simpleRender',
+		render: function() {
 			return React.DOM.div(null, "Rendering with ReactJS - ", this.props.framework);
+		}
+	}
+);
+
+var tableRender = React.createClass(
+	{
+		displayName: 'tableRender',
+		render: function() {
+			var data = this.props.data;
+
+			var rows = data.map(function (datum) {
+				var clickHandler = function (ev) {
+					console.log("Still in reactJs");
+					console.log(ev);
+				};
+
+				return (
+					React.DOM.tr(
+						{ 
+							onClick: clickHandler,
+							key: datum.key
+						},
+						React.DOM.td(null, datum['0']),
+						React.DOM.td(null, datum['1']),
+						React.DOM.td(null, datum['2']),
+						React.DOM.td(null, datum['3']),
+						React.DOM.td(null, datum['4'])
+					)
+				);
+			});
+
+			return (
+				React.DOM.table(null, rows)
+			);
 		}
 	}
 );
